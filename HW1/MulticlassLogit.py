e13a87b672dfa249eef1cf3e9fd1e0831f7e683b
ljtheminister <john.c.min@gmail.com>
2014-02-14 07:54:20 -0500

    working on it again

--- HW1/MulticlassLogit.py
+++ HW1/MulticlassLogit.py
@@ -22,9 +22,11 @@ def getPCA(base_dir):
     princomps = pd.read_csv(base_dir + 'Q.csv', header=None)
     return princomps
 
-def update(w, X, K, alpha, lamb):
+def update(w, X, N, K, alpha, lamb):
 
-
+    gradient = np.zeros(K)
+    for i in range(N):
+		
 
     w_new = w - alpha * gradient 
     return w_new
@@ -35,12 +37,15 @@ def gradient(X, y, alpha, eps):
     N, K = np.shape(X)
     idx = range(N)    
     w = np.zeros(N)
-    
-    w_new = update(w, x, alpha, K) 
+    w_new = update(w, X, N, K, alpha)
 
+    # repeat until convergence
     while np.linalg.norm(max(np.abs(w_new - w) > eps)):
 	w = w_new
-	w_new = update(w, X, alpha, K)
+	w_new = update(w, X, N, K, alpha)
+
+    return w
+
 def main():
     base_dir = '/Users/LJ/BMML/HW1/mnist_csv/'
     X_train, y_train = readMNISTdata(base_dir, 'train')
@@ -49,5 +54,6 @@ def main():
     eps = 1e-2
 
     w = gradient(X_train, y_train, alpha, eps) 
-
+    
+    pred_probs = np.dot(X_test.transpose(), w)
 
