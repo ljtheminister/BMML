1c605b0094a8c61424c7911c833afb8cb92d8df0
ljtheminister <john.c.min@gmail.com>
2014-02-26 01:54:39 -0500

    working on V_i, U_i

--- matrix_complete_MAP.py
+++ matrix_complete_MAP.py
@@ -1,13 +1,14 @@
 import numpy as np
-import os
+import subprocess
 
 
 # parameterization
-sigma = 0.25
+sigma = np.sqrt(0.25)
 lamb = 10
 
 d_list = [10, 20, 30]
 d = 10
+I = np.identity(d)
 
 N = 5
 ratings_range = np.array([1 + x for x in xrange(N)])
@@ -22,7 +23,7 @@ def load_data(filename):
     with open(filename, 'rb') as f:
 	for line in f:
 	    row = line.split(',')
-	    X.append((int(row[0]), int(row[1])))
+	    X.append((int(row[0])-1, int(row[1])-1))
 	    y.append(int(row[2]))
     return X, y
 
@@ -32,11 +33,13 @@ def load_data_dict(filename):
     with open(filename, 'rb') as f:
 	for line in f:
 	    row = line.split(',')
-	    user, movie, rating = int(row[0]), int(row[1]), int(row[2])
+	    user, movie, rating = int(row[0])-1, int(row[1])-1, int(row[2])
 	    data[(user, movie)] = rating
     return data
 
 
+
+def gibbs_sampler():
 
 
 
@@ -66,7 +69,70 @@ M = 1682
 
 mat = np.zeros((N,M))
 for user, movie in train.keys():
-    mat[user-1][movie-1] = train[(user, movie)]
+    mat[user, movie] = train[(user, movie)]
+
+
+user_movie_dict = dict()
+movie_user_dict = dict()
+
+for user_id in xrange(N):
+    user_movie_dict[user_id] = list()
+    for movie_id in xrange(M):
+	try:
+	    train[(user_id, movie_id)]
+	    user_movie_dict[user_id].append(movie_id)
+	except:
+	    pass
+
+
+for movie_id in xrange(M):
+    movie_user_dict[movie_id] = list()
+    for user_id in xrange(N):
+	try:
+	    train[(user_id, movie_id)]
+	    movie_user_dict[movie_id].append(user_id)
+	except:
+	    pass
+
+
+
+
+
+
+for i in xrange(N):
+
+    # compute V_i
+    V_i = 
+
+    # compute m_u
+    m_u = mat[i, user_movie_dict[i]]
+
+    # compute u_MAP
+    u_MAP = np.dot(np.linalg.inv(lamb*np.power(sigma,2)*I + np.dot(V.transpose(), V)), np.dot(V.transpose, m_u))
+
+
+for j in xrange(M):
+    # compute U
+    U_i = 
+
+    # compute m_v
+    m_v = mat[movie_user_dict[j], j]
+
+    # compute v_MAP
+    v_MAP = np.dot(np.linalg.inv(lamb*np.power(sigma,2)*I + np.dot(U.transpose(), U)), np.dot(U.transpose(), m_v))
+
+
+
+
+
+
+N_iterations = 100
+
+
+N_Gibbs = 500
+burn_in = 250
+thinning = 25
+
 
 
 
