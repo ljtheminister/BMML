95852177ea874a95f90c769e44832eb0723d2e4f
ljtheminister <john.c.min@gmail.com>
2014-02-27 00:01:52 -0500

    writing up functions

--- matrix_complete_MAP.py
+++ matrix_complete_MAP.py
@@ -37,9 +37,103 @@ def load_data_dict(filename):
 	    data[(user, movie)] = rating
     return data
 
+def metadata(data, base_dir):
+    users = list()
+    movies = list()
+    for user, movie in data.keys():
+	users.append(user)
+	movies.append(movie)
+    users = sorted(list(set(users)))
+    movies = sorted(list(set(movies)))
+    N = len(users)
+    M = int(subprocess.Popen('wc -l %s/movies.txt'%(base_dir), stdout=subprocess.PIPE, shell=True).communicate()[0].split()[0])
+    #M = 1682
+    return users, movies, N,M
+
+def user_movie_dictionary(data, N, M):
+    user_movie_dict = dict()
+    for user_id in xrange(N):
+	user_movie_dict[user_id] = list()
+	for movie_id in xrange(M):
+	    try:
+		data[(user_id, movie_id)]
+		user_movie_dict[user_id].append(movie_id)
+	    except:
+		pass
+    return user_movie_dict
+
+def movie_user_dictionary(data, N, M):
+    movie_user_dict = dict()
+    for movie_id in xrange(M):
+	movie_user_dict[movie_id] = list()
+	for user_id in xrange(N):
+	    try:
+		data[(user_id, movie_id)]
+		movie_user_dict[movie_id].append(user_id)
+	    except:
+		pass
+    return movie_user_dict
+
+#initialization
+def initialize_factorization(N, M, d):
+    U = np.zeros((N,d))
+    V = np.zeros((d,M))
+    mean = np.zeros(d)
+    cov = np.power(sigma,2)*I
+    for i in xrange(N):
+	U[i,:] = np.random.multivariate_normal(mean, cov)
+    for j in xrange(M):
+	V[:,j] = np.random.multivariate_normal(mean, cov)
+    return U, V
+
+# user optimization
+def update_user_MAP(U, V, N):
+    for i in xrange(N):
+	# compute V_i
+	V_i = V[:, user_movie_dict[i]].transpose()
+	# compute m_u
+	m_u = mat[i, user_movie_dict[i]]
+	# compute u_MAP
+	U[i,:] = np.dot(np.linalg.inv(lamb*np.power(sigma,2)*I + np.dot(V_i.transpose(), V_i)), np.dot(V_i.transpose(), m_u))
+    return U
+
+# movie optimizaiton
+def update_movie_MAP(U, V, M):
+    for j in xrange(M):
+	# compute U
+	U_j = U[movie_user_dict[j], :].transpose()
+	# compute m_v
+	m_v = mat[movie_user_dict[j], j]
+	# compute v_MAP
+	V[:,j] = np.dot(np.linalg.inv(lamb*np.power(sigma,2)*I + np.dot(U_j, U_j.transpose())), np.dot(U_j, m_v))
+    return V
+
+# coordinate ascent 
+def coordinate_ascent(N, M, d, N_iterations):
+    U, V = initialize_factorization(N, M, d)
+    for iter in xrange(N_iterations):
+	U = update_user_MAP(U, V, N)    
+	V = update_user_MAP(U, V, M)
+    return U, V
+
+def predict_ratings(U, V):
+    M_pred = np.dot(U,V)      
+    return M_pred      
+
+def RMSE(M_pred, M_test, ratings_range):  
+    MSE_sum = 0
+    N = len(M_test.keys())
+    for i, j in M_test.keys():
+	y_pred = map_to_ratings_N(M_pred[i,j], ratings_range)
+	y = M_test[i,j]
+	MSE_sum += np.power(y - y_pred, 2)
+    RMSE = np.sqrt(MSE_sum/float(N)) 
+    return RMSE
 
 
-def gibbs_sampler():
+
+
+
 
 
 
@@ -53,86 +147,19 @@ X_train, y_train = load_data(base_dir + 'ratings.txt')
 X_test, y_test = load_data(base_dir + 'ratings_test.txt')
 '''
 
-train = load_data_dict(os.path.join(base_dir, 'ratings.txt'))
-test = load_data_dict(os.path.join(base_dir, 'ratings_test.txt'))
+M_train = load_data_dict(os.path.join(base_dir, 'ratings.txt'))
+M_test = load_data_dict(os.path.join(base_dir, 'ratings_test.txt'))
 
-users = list()
-movies = list()
-
-for user, movie in train.keys():
-    users.append(user)
-    movies.append(movie)
-
-users = sorted(list(set(users)))
-movies = sorted(list(set(movies)))
-
-N = len(users)
-M = int(subprocess.Popen('wc -l %s/movies.txt'%(base_dir), stdout=subprocess.PIPE, shell=True).communicate()[0].split()[0])
-M = 1682
 
 mat = np.zeros((N,M))
-for user, movie in train.keys():
-    mat[user, movie] = train[(user, movie)]
+for user, movie in M_train.keys():
+    mat[user, movie] = M_train[(user, movie)]
 
 
-user_movie_dict = dict()
-movie_user_dict = dict()
+user_movie_dict = user_movie_dictionary(train, N, M)
+movie_user_dict = movie_user_dictionary(train, N, M)
 
-for user_id in xrange(N):
-    user_movie_dict[user_id] = list()
-    for movie_id in xrange(M):
-	try:
-	    train[(user_id, movie_id)]
-	    user_movie_dict[user_id].append(movie_id)
-	except:
-	    pass
-
-
-for movie_id in xrange(M):
-    movie_user_dict[movie_id] = list()
-    for user_id in xrange(N):
-	try:
-	    train[(user_id, movie_id)]
-	    movie_user_dict[movie_id].append(user_id)
-	except:
-	    pass
-
-
-#initialization
-U = np.zeros((N,d))
-V = np.zeros((d,M))
-
-mean = np.zeros(d)
-cov = np.power(sigma,2)*I
-for i in xrange(N):
-    U[i,:] = np.random.multivariate_normal(mean, cov)
-
-for j in xrange(M):
-    V[:,j] = np.random.multivariate_normal(mean, cov)
-
-
-# user optimization
-for i in xrange(N):
-    # compute V_i
-    V_i = V[:, user_movie_dict[i]].transpose()
-    # compute m_u
-    m_u = mat[i, user_movie_dict[i]]
-    # compute u_MAP
-    u_MAP = np.dot(np.linalg.inv(lamb*np.power(sigma,2)*I + np.dot(V_i.transpose(), V_i)), np.dot(V_i.transpose(), m_u))
-
-# movie optimizaiton
-for j in xrange(M):
-    # compute U
-    U_j = U[movie_user_dict[j], :].transpose()
-    # compute m_v
-    m_v = mat[movie_user_dict[j], j]
-    # compute v_MAP
-    v_MAP = np.dot(np.linalg.inv(lamb*np.power(sigma,2)*I + np.dot(U_j, U_j.transpose())), np.dot(U_j, m_v))
-
-
-
-
-
+N_iterations = 100
 
 
 ## GIBBS SAMPLING
