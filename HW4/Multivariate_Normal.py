b8d477556c1e3146390097a0832ab6c0a8d37ed7
ljtheminister <john.c.min@gmail.com>
2014-04-22 18:24:12 -0400

    building multivariate_normal class with pdf evaluation

--- HW4/Multivariate_Normal.py
+++ HW4/Multivariate_Normal.py
@@ -1,23 +1,25 @@
 import numpy as np
 import scipy
-
-
 from numpy import exp
 from numpy import trace
+from numpy import pi
 from numpy.linalg import det
 from numpy.linalg import pinv
 from numpy.linalg import inv
 
 class Multivariate_Normal:
-
     def __init__(self, k, mu, sigma):
 	self.k = k
 	self.mu = mu
 	self.sigma = sigma
 
-    def evaluate_pdf(x, k, mu, sigma):
-	Z = (2*pi)**(-k/2.0)*det(sigma)**(0.5)
-	return Z*exp(-0.5*(x-mu).transpose().pinv(sigma).dot(x-mu)
+	assert self.k = mu.shape[0]
+	assert.self.k = sigma.shape[0]
+	assert.self.k = sigma.shape[1]
 
+    def evaluate_pdf(x, k, mu, sigma):
+		Z = (2*pi)**(-k/2.0)*det(sigma)**(0.5) #normalization term
+	    pdf = Z*exp(-0.5*(x-mu).T.dot(pinv(sigma)).dot(x-mu))
+		return pdf
 
 
